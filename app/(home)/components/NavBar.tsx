91228bcc9f5ddeac13bc7fea276cec0002f2c75a
Ahmed Barhoumi <banoni.bro@gmail.com>
2024-07-21 17:21:55 +0100

    fixing some errors while building

--- app/(home)/components/NavBar.tsx
+++ app/(home)/components/NavBar.tsx
@@ -1,48 +1,43 @@
-"use client"
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { it } from 'node:test'
-import Link from 'next/link'
-import { ModeToggle } from '@/components/mode-toggle'
+"use client";
+import { ModeToggle } from "@/components/mode-toggle";
+import Link from "next/link";
 
 const navigation = [
-  { name: 'Home', href: '#', current: true },
-  { name: 'Services', href: '#', current: false },
-  { name: 'Pricing', href: '#', current: false },
-  { name: 'Login', href: '#', current: false },
-]
+  { name: "Home", href: "#", current: true },
+  { name: "Services", href: "#", current: false },
+  { name: "Pricing", href: "#", current: false },
+  { name: "Login", href: "#", current: false },
+];
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function NavBar() {
   return (
-    <nav className='w-full backdrop-blur-lg h-[70px] m-auto flex justify-between item-center border-b border-[rgba(255,255,255,0.01)] z-40 fixed'>
-     <div className='w-[70vw] m-auto flex justify-between items-center'>
-         <div className="left flex items-center">
-            LOGO
-        </div>
+    <nav className="w-full backdrop-blur-lg h-[70px] m-auto flex justify-between item-center border-b border-[rgba(255,255,255,0.01)] z-40 fixed">
+      <div className="w-[70vw] m-auto flex justify-between items-center">
+        <div className="left flex items-center">LOGO</div>
         <div className="flex items-center right">
-          <ul className='flex justify-center items-center'>
-              {navigation.map((item, itemIdx) => (
-                <li><Link
-                    key={itemIdx}
-                    href={item.href}
-                    className={classNames(
-                        item.current ? 'text-blue' : 'text-gray-500',
-                        'px-8 py-5 text-l font-medium cursor-pointer'
-                    )}
-                    aria-current={item.current ? 'page' : undefined}
+          <ul className="flex justify-center items-center">
+            {navigation.map((item, itemIdx) => (
+              <li key={itemIdx}>
+                <Link
+                  href={item.href}
+                  className={classNames(
+                    item.current ? "text-blue" : "text-gray-500",
+                    "px-8 py-5 text-l font-medium cursor-pointer",
+                  )}
+                  aria-current={item.current ? "page" : undefined}
                 >
-                    {item.name}
-                </Link></li>
+                  {item.name}
+                </Link>
+              </li>
             ))}
-            </ul> 
-            <ModeToggle />
+          </ul>
+          <ModeToggle />
         </div>
-      </div> 
+      </div>
     </nav>
-  )
+  );
 }
--- components/pricing-container.tsx
+++ components/pricing-container.tsx
@@ -1,90 +1,77 @@
 "use client";
 
-import React, { useState } from 'react'
-import clsx from 'clsx';
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
+import React from "react";
 const plans = [
-    {
-        "name": "Basic",
-        "active": false,
-        "price": 1,
-        "features": {
-            "storage": "24/ 7 Support"
-        }
+  {
+    name: "Basic",
+    active: false,
+    price: 1,
+    features: {
+      storage: "24/ 7 Support",
     },
-  
+  },
 ];
 
 export function PropDivider() {
-    return (
-      <div className="w-full h-[0.015rem] bg-LightGrayishBlue/50"
-      ></div>
-    )
-  }
-  
-type PlanProps = (typeof plans)[number];
-
-export function Plan({
-	name,
-	active,
-	price,
-	features,
-}: PlanProps ) {
-
-	let planClass = `flex flex-col justify-start items-center rounded-[0.65rem] px-[1.9rem] font-bold  ${
-		(active) ? "h-[31.25rem] w-[21.875rem] bg-customGradientBottom py-[3.4rem] text-white" : "h-[28.4rem] w-[21.875rem] py-[1.9rem] bg-white text-"
-	}`;
-
-
-	return (
-		<section className={planClass}>
-			
-			<h2 className="text-lg">{name}</h2>
-			
-			<h1 className={`flex items-center gap-[0.7rem] text-xl -tracking-[0.03em] mt-[1.45rem] mb-[2.1rem] ${
-				(active)? "text-white" : "text-DarkGrayishBlue"
-			}`}>100 Credits -->{" "}
-				{price}$
-			</h1>
-			
-			<div className="flex flex-col items-center gap-[0.95rem] w-full text-[0.95rem]">
-				<PropDivider />
-				<h3>{features.storage}</h3>
-				<PropDivider />
-				<PropDivider />
-			</div>
-
-			<button className={`h-[2.75rem] w-full text-xs tracking-[.17em] rounded-md mt-[1.95rem] uppercase border-[0.1rem] ${
-				(active)? "active-btn" : "gradient-btn"
-			}`}>
-				Learn more
-			</button>
-
-		</section>
-	)
+  return <div className="w-full h-[0.015rem] bg-LightGrayishBlue/50"></div>;
 }
 
+type PlanProps = (typeof plans)[number];
 
+export function Plan({ name, active, price, features }: PlanProps) {
+  let planClass = `flex flex-col justify-start items-center rounded-[0.65rem] px-[1.9rem] font-bold  ${
+    active
+      ? "h-[31.25rem] w-[21.875rem] bg-customGradientBottom py-[3.4rem] text-white"
+      : "h-[28.4rem] w-[21.875rem] py-[1.9rem] bg-white text-"
+  }`;
 
+  return (
+    <section className={planClass}>
+      <h2 className="text-lg">{name}</h2>
+
+      <h1
+        className={`flex items-center gap-[0.7rem] text-xl -tracking-[0.03em] mt-[1.45rem] mb-[2.1rem] ${
+          active ? "text-white" : "text-DarkGrayishBlue"
+        }`}
+      >
+        100 Credits --&gt; {price}$
+      </h1>
+
+      <div className="flex flex-col items-center gap-[0.95rem] w-full text-[0.95rem]">
+        <PropDivider />
+        <h3>{features.storage}</h3>
+        <PropDivider />
+        <PropDivider />
+      </div>
+
+      <button
+        className={`h-[2.75rem] w-full text-xs tracking-[.17em] rounded-md mt-[1.95rem] uppercase border-[0.1rem] ${
+          active ? "active-btn" : "gradient-btn"
+        }`}
+      >
+        Learn more
+      </button>
+    </section>
+  );
+}
 
 export function Plans() {
   return (
-    <motion.div 
+    <motion.div
       className="flex flex-col gap-8 mb-20 items-center pt-[4rem] lg:flex-row lg:gap-0 lg:mb-0 drop-shadow-purple"
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{
-          type: "tween",
-          duration: 0.2
+        type: "tween",
+        duration: 0.2,
       }}
     >
-        {
-            plans.map((plan, index) => (
-                <React.Fragment key={index}>
-                    <Plan {...plan}/>
-                </React.Fragment>
-            ))
-        }
+      {plans.map((plan, index) => (
+        <React.Fragment key={index}>
+          <Plan {...plan} />
+        </React.Fragment>
+      ))}
     </motion.div>
-  )
+  );
 }
