951d91ae8fac50e932f2849362b945bc7ecb720b
Ahmed Barhoumi <banoni.bro@gmail.com>
2024-08-10 22:30:38 +0100

    fix(api/transactions): import NextApiRequest and NextApiResponse types for better type safety and clarity in the API handler fix(api/transactions): specify error type as 'any' in catch block to improve TypeScript type handling

--- src/pages/api/transactions/index.ts
+++ src/pages/api/transactions/index.ts
@@ -1,5 +1,5 @@
-import from "next";
 import { processOrder } from "@/services/transactionService";
+import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,7 +10,7 @@ export default async function handler(
       const { userId, subserviceId, amount } = req.body;
       const order = await processOrder({ userId, subserviceId, amount });
       res.status(201).json(order);
-    } catch (error) {
+    } catch (error: any) {
       console.error("Error processing order:", error);
       res
         .status(500)
