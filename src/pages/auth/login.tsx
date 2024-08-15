f8d56fe36bcaf5784dad3dddaa955e1980e1bf20
Ahmed Barhoumi <banoni.bro@gmail.com>
2024-08-15 21:25:55 +0100

    feat(auth): add SignInForm component and integrate it into the login page to provide user authentication functionality

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,9 @@
+import SignInForm from "@/components/signin-form";
+
+export default function Register() {
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <SignInForm />
+    </div>
+  );
+}
